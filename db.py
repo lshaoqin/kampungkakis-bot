ce42ea1b6e4c80feff661401ab2dae724c6cc13b
lshaoqin <shaoqin.lu@gmail.com>
2022-12-31 15:38:23 +0800

    update database schema to include users

--- db.py
+++ db.py
@@ -6,24 +6,46 @@ app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.sqlite3'
 db = SQLAlchemy(app)
 
 def save_msg(name, hp_no, whatsapp_id, body):
-   msg = message(name = name, hp_no = hp_no, whatsapp_id = whatsapp_id, body = body)
+   msg = Message(name = name, hp_no = hp_no, whatsapp_id = whatsapp_id, body = body)
    db.session.add(msg)
    db.session.commit()
-   
-class message(db.Model):
+
+def save_user(hp_no, state):
+   newUser = User(hp_no = hp_no, state = state)
+   db.session.add(newUser)
+   db.session.commit()
+
+def check_user(hp_no):
+   #Check user by phone number
+   user = User.query.filter_by(hp_no = hp_no).first()
+   if user:
+      #if user exists, return their state
+      return user.state
+   else:
+      #if user does not exist, save the user and return state as default
+      save_user(hp_no, 'default')
+      return 'default'
+
+#Edit user state, returns True if successful and False otherwise
+def edit_user(hp_no, new_state):
+   user = User.query.filter_by(hp_no=hp_no).first()
+   if user:
+      user.state = new_state
+      db.session.commit()
+      return True
+   return False
+
+class Message(db.Model):
    id = db.Column('message_id', db.Integer, primary_key = True)
    name = db.Column(db.String(100))
    hp_no = db.Column(db.String(12))  
    whatsapp_id = db.Column(db.String(30))
    body = db.Column(db.String(5000))
 
-
-def __init__(self, name, hp_no, whatsapp_id, body):
-   self.name = name
-   self.hp_no = hp_no
-   self.whatsapp_id = whatsapp_id
-   self.body = body
-
+class User(db.Model):
+   id = db.Column('user_id', db.Integer, primary_key = True)
+   hp_no = db.Column(db.String(12)) 
+   state = db.column(db.String(50))
 
 
 with app.app_context():
